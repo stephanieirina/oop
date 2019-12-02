ab8b68fc644c04c80a04642e73628db5ff11da3c
Jean-Luc <jean-luc.a@ideafactory.fr>
2019-12-02 08:10:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# typescript-oop
+Modèles de classes et concepts objets avec Typescript
+
+# Installation
+$ git clone https://github.com/dacodemaniak/typescript-oop.git <your_workspace_folder>
+$ npm init
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'app.js',
+        chunkFilename: "app.chunk.js"
+    },
+    optimization: {
+        minimizer: [new TerserPlugin()],
+        splitChunks: {
+            chunks: 'all'
+        },
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.js',
+            '.tsx'
+        ]
+    },
+    devtool: 'source-map',
+};
